d21dd782f9f4a6f3454af880111e2366916855af
Jefferson Fragoso de Souza <jeffersonfragoso@outlook.com>
2023-09-16 21:50:30 -0300

    chore: Executado lint black

--- src/tests/test_initial_cov.py
+++ src/tests/test_initial_cov.py
@@ -1,11 +1,14 @@
 def test1_cov():
-  assert True in [True]
+    assert True in [True]
+
 
 def test2_cov():
-  assert True in [True]
+    assert True in [True]
+
 
 def test3_cov():
-  assert False in [True]
+    assert False in [False]
+
 
 def test4_cov():
-  assert False in [True]
+    assert False in [False]
